fa93b12bed338c5cea15f2717e9fa21f67fcc341
Azaan Suhail <azaan.suhail@gmail.com>
2025-08-06 17:21:54 +0530

    bug fixing 1.0.7

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: path.resolve(__dirname), // This makes sure it uses the current folder (frontend/frontend) as root
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    outDir: path.resolve(__dirname, 'dist'),
-    emptyOutDir: true,
-  },
-})
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},
+});
