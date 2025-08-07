15ceffdd6e2e3e21b3df10d0ddd585f69ea0bc51
Azaan Suhail <azaan.suhail@gmail.com>
2025-08-07 12:58:49 +0530

    bug fixing 1.0.24

--- frontend/src/stores/useCartStore.js
+++ frontend/src/stores/useCartStore.js
@@ -58,7 +58,7 @@ export const useCartStore = create((set, get) => ({
 				const newCart = existingItem
 					? prevState.cart.map((item) =>
 							item._id === product._id ? { ...item, quantity: item.quantity + 1 } : item
-					  )
+					)
 					: [...prevState.cart, { ...product, quantity: 1 }];
 				return { cart: newCart };
 			});
